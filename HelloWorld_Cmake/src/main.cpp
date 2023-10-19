c12d4e526d268920312c854d7c586781eaade23d
khtb <mahmoud.khattab@de.bosch.com>
2023-10-19 12:37:28 +0200

    rename old main.c to sc_rvd.c

--- HelloWorld_Cmake/src/main.cpp
+++ HelloWorld_Cmake/src/main.cpp
@@ -37,124 +37,36 @@
   //  Andy Goodrich: new example using a ready-valid handshake for communication.
   //
 
-#include "systemc.h"
-// #include <systemc>
+// #include "systemc.h"
+#include <systemc>
 using namespace sc_core;
-using namespace std;
-#include <iomanip>
-#include "sc_rvd.h"
 #include "main.hpp"
 
-SC_MODULE(DUT)
-{
-    SC_CTOR(DUT)
-    {
-        SC_CTHREAD(thread, m_clk.pos());
-        reset_signal_is(m_reset, false);
-    }
-    void thread()
-    {
-        sc_uint<8> data[10];
 
-        m_input.reset();
-        m_output.reset();
-        wait();
-        for (;;)
-        {
-            for (int outer_i = 0; outer_i < 10; outer_i++)
-            {
-                for (int inner_i = 0; inner_i < outer_i; inner_i++)
-                {
-                    data[inner_i] = m_input.read();
-                    cout << "         " << std::setw(3) << data[inner_i]
-                        << "          " << sc_time_stamp() << endl;
-                }
-                for (int inner_i = 0; inner_i < outer_i; inner_i++)
-                {
-                    m_output = data[inner_i];
-                }
-            }
-        }
+
+void hello1(void)
+{
+    std::cout<<"hello world #1";
+}
+
+struct HelloWorld:sc_module 
+{
+    SC_CTOR(HelloWorld)
+    {
+        SC_METHOD(hello2);
     }
-    sc_in<bool>              m_clk;
-    sc_rvd<sc_uint<8> >::in  m_input;
-    sc_rvd<sc_uint<8> >::out m_output;
-    sc_in<bool>              m_reset;
+    void hello2()
+    {
+        std::cout<<name()<<"hello world #2"<<std::endl;
+    }
+
 };
 
-SC_MODULE(TB)
+
+int sc_main(int,char*[])
 {
-    SC_CTOR(TB)
-    {
-        SC_CTHREAD(consumer, m_clk.pos());
-        reset_signal_is(m_reset, false);
-        SC_CTHREAD(producer, m_clk.pos());
-        reset_signal_is(m_reset, false);
-    }
-
-    void consumer()
-    {
-        sc_uint<8> data;
-
-        m_from_dut.reset();
-        wait();
-
-        for (int i = 0; i < 40; i++)
-        {
-            data = m_from_dut.read();
-            cout << "               " << std::setw(3) << data << "    "
-                << sc_time_stamp() << endl;
-        }
-        sc_stop();
-    }
-
-    void producer()
-    {
-        sc_uint<8> data;
-
-        m_to_dut.reset();
-        wait();
-        for (int i = 0;; i++)
-        {
-            cout << "  " << std::setw(3) << i << "                 "
-                << sc_time_stamp() << endl;
-            data = i;
-            m_to_dut = data;
-            if (i && (i % 6 == 0)) wait(i);
-        }
-    }
-
-    sc_in<bool>              m_clk;
-    sc_rvd<sc_uint<8> >::in  m_from_dut;
-    sc_in<bool>              m_reset;
-    sc_rvd<sc_uint<8> >::out m_to_dut;
-};
-
-int sc_main(int, char* [])
-{
-    sc_clock            clock;
-    DUT                 dut("dut");
-    sc_rvd<sc_uint<8> > dut_to_tb;
-    sc_signal<bool>     reset;
-    TB                  tb("tb");
-    sc_rvd<sc_uint<8> > tb_to_dut;
-
-    dut.m_clk(clock);
-    dut.m_reset(reset);
-    dut.m_input(tb_to_dut);
-    dut.m_output(dut_to_tb);
-
-    tb.m_clk(clock);
-    tb.m_reset(reset);
-    tb.m_from_dut(dut_to_tb);
-    tb.m_to_dut(tb_to_dut);
-
-    cout << "producer dut consumer " << endl;
-    reset = false;
-    sc_start(1, SC_NS);
-    reset = true;
+    hello1();
+    HelloWorld x("y");
     sc_start();
-
-    cout << "Program completed" << endl;
     return 0;
 }
